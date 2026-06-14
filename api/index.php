6db297bcd4cff966c098bdddf56131f4d1ec3eb9
Victor <faiqalmuhajid@gmail.com>
2026-06-14 15:23:30 +0700

    fix vercel runtime directories

--- api/index.php
+++ api/index.php
@@ -1,3 +1,17 @@
 <?php
 
+$paths = [
+    '/tmp/views',
+    '/tmp/framework',
+    '/tmp/framework/cache',
+    '/tmp/framework/sessions',
+    '/tmp/framework/views',
+];
+
+foreach ($paths as $path) {
+    if (! is_dir($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
