aa2dada3fa7255ba50b4affb60da4abc38adf5f1
Victor <faiqalmuhajid@gmail.com>
2026-06-15 09:52:51 +0700

    restore broken laravel core files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,31 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
-use Livewire\Livewire;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceRootUrl(config('app.url'));
-            URL::forceScheme('https');
-        }
-
-        Livewire::setScriptRoute(function ($handle) {
-            return Route::get('/build/livewire/livewire.min.js', $handle);
-        });
-
-        Livewire::setUpdateRoute(function ($handle) {
-            return Route::post('/livewire/update', $handle);
-        });
+        //
     }
 }
